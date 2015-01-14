157e327eae897108e9053fa1551d84b6702fb803
Inseok Hwang <inseokhwang1@gmail.com>
2015-01-14 15:30:21 -0500

    Implemented keyboard input, basic GUI framework's working

--- Makefile
+++ Makefile
@@ -4,12 +4,13 @@
 #
 
 CC = gcc
-CFLAGS = -std=c99 -Werror
+CFLAGS = -std=c99 -Werror 
+LINKLINE = -lglut -lGLU -lGL
 EXECUTABLE = CCHIP-8
 
 default:	chip8.o
 
 chip8.o:	chip8.c
-		$(CC) $(CFLAGS) chip8.c
+		$(CC) $(CFLAGS) chip8.c $(LINKLINE)
 
 clean:		rm *.o
--- chip8.c
+++ chip8.c
@@ -5,6 +5,16 @@
 
 #define debug printf("debug\n");
 
+// Display size
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+#define MODIFIER 10
+
+// #define DRAWWITHTEXTURE
+
+/*
+ *      Variables for CPU
+ */
 FILE *rom;
 unsigned short maxRomSize = 3584;
 
@@ -53,6 +63,16 @@ unsigned char getX(unsigned short i);
 unsigned char getY(unsigned short i);
 unsigned char getNN(unsigned short i);
 
+/*
+ *      Variables for GUI
+ */
+int display_width = SCREEN_WIDTH * MODIFIER;
+int display_height = SCREEN_HEIGHT * MODIFIER;
+
+typedef unsigned char u8;
+u8 screenData [SCREEN_HEIGHT] [SCREEN_WIDTH] [3];
+
+void reshape_window(GLsizei w, GLsizei h);
 
 /*
  *
@@ -72,6 +92,7 @@ void fp0 (unsigned short i) {
                                         gfx[x + 64*y] = 0;
                                 }
                         }
+                        drawFlag = 1;
                         break;
                 // Return from subroutine
                 case 0x0EE:
@@ -212,21 +233,21 @@ void fpD (unsigned short i) {
         X = reg[X];
         Y = reg[Y];
         unsigned char N = i & 0xF;
-        short pixelCounter = 0;
         reg[0xF] = 0;
+        unsigned char pixel;
         for (char y = 0; y < N; ++y) {
+                pixel = mem[I + y];
                 for (char x = 0; x < 8; ++x) {
                         int pixelIndex = X + x + 64*(Y + y);
-                        if (pixelIndex >= 2048)
-                                pixelIndex - 2048;
-                        gfx[pixelIndex] += mem[I + pixelCounter];
-                        pixelCounter++;
-                        if (gfx[pixelIndex] > 1) {
-                                gfx[pixelIndex] = 0;
-                                reg[0xF] = 1;
+                        if (pixel & (0x80 >> x)) {
+                                if (gfx[pixelIndex]) {
+                                        reg[0xF] = 1;
+                                }
+                                gfx[pixelIndex] ^= pixel & 0xF;
                         }
                 }
         }
+        drawFlag = 1;
 }
 
 /* 
@@ -322,24 +343,165 @@ unsigned char getNN (unsigned short i) {
 
 /*
  *
- *     Main methods used for emulation
- *
+ *      Methods to help set up the graphics
  *
  */
 
- void initGraphics() {
+void runCycle();
+void setInput();
 
+void initTexture() {
+        // Clear screen
+        for(int y = 0; y < SCREEN_HEIGHT; ++y)              
+                for(int x = 0; x < SCREEN_WIDTH; ++x)
+                        screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;
+
+        // Create a texture 
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
+
+        // Set up the texture
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP); 
+
+        // Enable textures
+        glEnable(GL_TEXTURE_2D);
+}
+
+void drawPixel(int x, int y)
+{
+        glBegin(GL_QUADS);
+                glVertex3f((x * MODIFIER) + 0.0f,     (y * MODIFIER) + 0.0f,     0.0f);
+                glVertex3f((x * MODIFIER) + 0.0f,     (y * MODIFIER) + MODIFIER, 0.0f);
+                glVertex3f((x * MODIFIER) + MODIFIER, (y * MODIFIER) + MODIFIER, 0.0f);
+                glVertex3f((x * MODIFIER) + MODIFIER, (y * MODIFIER) + 0.0f,     0.0f);
+        glEnd();
+}
+
+void updateTexture() {
+        for(int y = 0; y < 32; ++y)            
+                for(int x = 0; x < 64; ++x)
+                        if(gfx[(y * 64) + x] == 0)
+                                screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;    // Disabled
+                        else 
+                                screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 255;  // Enabled
+
+        // Update Texture
+        glTexSubImage2D(GL_TEXTURE_2D, 0 ,0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
+
+        glBegin( GL_QUADS );
+                glTexCoord2d(0.0, 0.0);         glVertex2d(0.0, 0.0);
+                glTexCoord2d(1.0, 0.0);         glVertex2d(display_width, 0.0);
+                glTexCoord2d(1.0, 1.0);         glVertex2d(display_width, display_height);
+                glTexCoord2d(0.0, 1.0);         glVertex2d(0.0, display_height);
+        glEnd();
  }
 
- void initInput() {
+void updateQuads()
+{
+        // Draw
+        for(int y = 0; y < 32; ++y)             
+                for(int x = 0; x < 64; ++x)
+                {
+                        if(gfx[(y*64) + x] == 0) 
+                                glColor3f(0.0f,0.0f,0.0f);                      
+                        else 
+                                glColor3f(1.0f,1.0f,1.0f);
 
- }
+                        drawPixel(x, y);
+                }
+}
+
+void drawScreen() {
+        runCycle();
+        if(drawFlag) {
+                glClear(GL_COLOR_BUFFER_BIT);
+
+#ifdef DRAWWITHTEXTURE
+                updateTexture();
+#else
+                updateQuads();
+#endif
+                glutSwapBuffers();
+
+                drawFlag = 0;
+        }
+}
+
+void reshape_window(GLsizei w, GLsizei h)
+{
+    glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, w, h, 0);        
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, w, h);
+ 
+    // Resize quad
+    display_width = w;
+    display_height = h;
+}
+
+void keyboardDown(unsigned char k, int x, int y)
+{
+        if(k == 27)    // esc
+                exit(0);
+
+        if(k == '1')  key[0x1] = 1;
+        if(k == '2')  key[0x2] = 1;
+        if(k == '3')  key[0x3] = 1;
+        if(k == '4')  key[0xC] = 1;
+
+        if(k == 'q')  key[0x4] = 1;
+        if(k == 'w')  key[0x5] = 1;
+        if(k == 'e')  key[0x6] = 1;
+        if(k == 'r')  key[0xD] = 1;
+
+        if(k == 'a')  key[0x7] = 1;
+        if(k == 's')  key[0x8] = 1;
+        if(k == 'd')  key[0x9] = 1;
+        if(k == 'f')  key[0xE] = 1;
+
+        if(k == 'z')  key[0xA] = 1;
+        if(k == 'x')  key[0x0] = 1;
+        if(k == 'c')  key[0xB] = 1;
+        if(k == 'v')  key[0xF] = 1;
+}
+
+void keyboardUp(unsigned char k, int x, int y)
+{
+        if(k == '1')  key[0x1] = 0;
+        if(k == '2')  key[0x2] = 0;
+        if(k == '3')  key[0x3] = 0;
+        if(k == '4')  key[0xC] = 0;
+
+        if(k == 'q')  key[0x4] = 0;
+        if(k == 'w')  key[0x5] = 0;
+        if(k == 'e')  key[0x6] = 0;
+        if(k == 'r')  key[0xD] = 0;
+
+        if(k == 'a')  key[0x7] = 0;
+        if(k == 's')  key[0x8] = 0;
+        if(k == 'd')  key[0x9] = 0;
+        if(k == 'f')  key[0xE] = 0;
+
+        if(k == 'z')  key[0xA] = 0;
+        if(k == 'x')  key[0x0] = 0;
+        if(k == 'c')  key[0xB] = 0;
+        if(k == 'v')  key[0xF] = 0;
+}
+
+
+/*
+ *
+ *      Main methods used for emulation
+ *
+ */
+
 
 //Initializes registers and memory
 void initialize() {
-        initGraphics();
-        initInput();
-
         PC = 0x200;
         opcode = 0;
         I = 0;
@@ -412,7 +574,6 @@ void printHelp() {
 // Fetch, Decode, Execute Opcode and Update timer
 void runCycle() {
         opcode = mem[PC] << 8 | mem[PC + 1];
-        printf("MEM[0x%X] = 0x%2X\n", PC, opcode);
         unsigned short i = opcode & 0xFFF;
         opcode = opcode >> 12;
         PC += 2;
@@ -427,12 +588,19 @@ void runCycle() {
                 --delay_timer;
 }
 
-void drawScreen() {
-
-}
-
-void setInput() {
-
+void debugDisplay() {
+        for(int y = 0; y < 32; ++y)
+        {
+                for(int x = 0; x < 64; ++x)
+                {
+                        if(gfx[(y*64) + x] == 0) 
+                                printf("O");
+                        else 
+                                printf(" ");
+                }
+                printf("\n");
+        }
+        printf("\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -443,12 +611,30 @@ int main(int argc, char *argv[]) {
                 printHelp();
         }
 
-        while(1) {
-                runCycle();
-                if(drawFlag) {
-                        drawScreen();
-                }
-                setInput();
-        }
+        // while (1) {
+        //         runCycle();
+        //         if (drawFlag) {
+        //                 debugDisplay();
+        //                 drawFlag = 0;
+        //         }
+        // }
+
+        glutInit(&argc, argv);          
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+        glutInitWindowSize(display_width, display_height);
+        glutInitWindowPosition(320, 320);
+        glutCreateWindow("CCHIP-8");
+
+        glutDisplayFunc(drawScreen);
+        glutIdleFunc(drawScreen);
+        glutReshapeFunc(reshape_window);
+        glutKeyboardFunc(keyboardDown);
+        glutKeyboardUpFunc(keyboardUp); 
+
+        updateTexture();
+
+        glutMainLoop(); 
+
         return 0;
 }
